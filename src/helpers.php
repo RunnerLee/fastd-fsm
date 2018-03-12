0b3f44ef8fd0cf2a18f05dc80aeca4c816250010
RunnerLee <runnerleer@gmail.com>
2018-03-12 14:36:08 +0800

    update helper

--- src/helpers.php
+++ src/helpers.php
@@ -4,9 +4,18 @@
  * @email: runnerleer@gmail.com
  * @time: 2018-03
  */
+
+use Runner\Heshen\Contracts\StatefulInterface;
+use Runner\Heshen\Machine;
+
 if (!function_exists('fsm')) {
-    function fsm()
+
+    /**
+     * @param StatefulInterface $stateful
+     * @return Machine
+     */
+    function fsm(StatefulInterface $stateful)
     {
-        return app()->get('fsm');
+        return app()->get('fsm')->make($stateful);
     }
 }
